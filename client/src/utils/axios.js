d31f8936adbca4cbeeb202a3cf3d0200317d3f45
ythapa586 <ythapa586@gmail.com>
2026-06-06 20:36:15 +0530

    Fix controller filename

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://railway.com/project/0317b899-b61b-4d18-960e-538a91cd44c2/service/c652985b-d214-4d62-aa81-84ade009d6ef?environmentId=f57b1d14-675a-4b0e-851a-da58998b231b&id=b7f86fec-fe66-45f0-826b-c6bc1f036f36#build/api",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
