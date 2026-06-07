93206c4580d250a860ee95913cb9ee28ec0326d0
ythapa586 <ythapa586@gmail.com>
2026-06-07 15:39:58 +0530

    Fix Render CORS and API URL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://eventora-fyxm.onrender.com",
+  baseURL: "https://eventora-fyxm.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
