ca061117a94726af25e7548c3af0229374185b15
ythapa586 <ythapa586@gmail.com>
2026-06-07 15:35:26 +0530

    Use Render backend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://eventora-production-9efc.up.railway.app/api",
+  baseURL: "https://eventora-fyxm.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
