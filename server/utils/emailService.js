450294c59745a069882a8c7beb47c2d70abdced1
ythapa586 <ythapa586@gmail.com>
2026-06-13 20:56:56 +0530

    Fix Gmail SMTP

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -4,11 +4,23 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR =", error);
+  } else {
+    console.log("SMTP READY");
+  }
 });
 
 exports.sendOtpEmail = async (email, otp, type) => {
