94942af6213afea7e42a0c5eff2caac965930d4e
Yashoda Thapa <ythapa586@gmail.com>
2026-06-13 21:56:24 +0530

    Update emailService.js

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -5,16 +5,15 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
+
 transporter.verify((error, success) => {
   if (error) {
     console.log("SMTP ERROR =", error);
